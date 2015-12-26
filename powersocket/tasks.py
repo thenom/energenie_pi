ce82762f62e55c34421ff6b2ec44040251f5d24b
Simon Thorley <simon.thorley@thenom.local>
2015-12-26 21:06:25 +0000

    Initial work on scheduling

--- powersocket/tasks.py
+++ powersocket/tasks.py
@@ -1,6 +1,7 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
-from powersocket.utils import loopcheck
+#from powersocket.utils import loopcheck
+from powersocket.models import Socket, Schedule
 from celery.utils.log import get_task_logger
 from datetime import datetime
 
@@ -11,7 +12,22 @@ logger = get_task_logger(__name__)
 # A periodic task that will run every minute (the symbol "*" means every)
 @periodic_task(run_every=(crontab(hour="*", minute="*", day_of_week="*")))
 def statecheck():
-    logger.info("Start task")
-    now = datetime.now()
-    result = loopcheck.check_state()
-    logger.info('Task finished: result = ' + result)
+    sockets = Socket.objects.all()
+    schedules = Schedule.objects.all()
+    for schedule in schedules:
+        now = datetime.now()
+        print schedule.TimeSlot.all()
+        print 'Checking schedule: ' + schedule.description
+
+        for timeslot in schedule.TimeSlot.all():
+            print 'Checking time slot: ' + schedule.start_time + '-' + schedule.end_time + ' (' + ', '.join(day.day for day in self.days_of_week.all()) + ')'
+            if now >= timeslot.start_time and now <= timeslot.end_time:
+                print ' - Schedule is active!'
+                for socket in sockets:
+                    print '...Checking light: ' + socket.name
+
+
+#    logger.info("Start task")
+#    now = datetime.now()
+#    result = loopcheck.check_state()
+#    logger.info('Task finished: result = ' + result)
