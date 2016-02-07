7fdc36d001b56e85630b86b2b7c6f28539b09afc
root <root@rpi-energenie.thenom.local>
2016-02-07 06:01:19 -0500

    Corrected missing post data on html template

--- energenie_pi/settings.py
+++ energenie_pi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@ww$#1*ke3khx9eb@36bmmn&71ihb++f$tv#6t7-=*z$x_zgo('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '192.168.101.147' ]
 
 
 # Application definition
