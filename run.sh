092d24a7744aa7fdaf06414dde7b674e55575a00
Simon Thorley <simon.thorley@thenom.local>
2016-05-01 12:53:13 +0100

    Added standard run.sh

--- run.sh
+++ run.sh
@@ -1,55 +1,37 @@
 #!/bin/bash
 
-CWD="/usr/local/energenie_pi/"
-PYTHON=/usr/bin/python
-BINDADDR=0.0.0.0
-PORT=8000
+source run_config
 
 # setup days of the week
 $PYTHON "${CWD}manage.py" loaddata AstralCities.json
 
-$PYTHON "${CWD}manage.py" celerybeat &
-BEATPID=$!
-echo "Started Celerybeat [${BEATPID}]"
-$PYTHON "${CWD}manage.py" celeryd &
-CELERYPID=$!
-echo "Started CeleryD [${CELERYPID}]"
 $PYTHON "${CWD}manage.py" runserver ${BINDADDR}:${PORT} &
 SERVERPID=$!
 echo "Started Server [${SERVERPID}]"
+$PYTHON "${CWD}sched_run.py" ${CHECKINTER} ${BINDADDR} &
+CHECKPID=$!
+echo "Started scheduler [${CHECKPID}]"
 
 trap ctrl_c INT
 
 function ctrl_c() {
-    kill ${CELERYPID}
     kill ${SERVERPID}
-    kill ${BEATPID}
+    kill ${CHECKPID}
     kill $(ps aux | grep 'manage.py runserver' | grep -v grep | awk '{print $2}')
     exit 0
 }
 
 while true; do
-    kill -s 0 ${BEATPID}
-    if [ $? -ne 0 ]; then
-        echo "Celerybeat has died..."
-        kill ${CELERYPID}
-        kill ${SERVERPID}
-        killall $(ps aux | grep 'manage.py runserver' | awk '{print $2}')
-        exit 1
-    fi
-    kill -s 0 ${CELERYPID}
-    if [ $? -ne 0 ]; then
-        echo "CeleryD has died..."
-        kill ${BEATPID}
-        kill ${SERVERPID}
-        killall $(ps aux | grep 'manage.py runserver' | awk '{print $2}')
-        exit 1
-    fi
     kill -s 0 ${SERVERPID}
     if [ $? -ne 0 ]; then
         echo "Server has died..."
-        kill ${CELERYPID}
-        kill ${BEATPID}
+	kill ${CHECKPID}
+        exit 1
+    fi
+    kill -s 0 ${CHECKPID}
+    if [ $? -ne 0 ]; then
+        echo "Scheduled check has died..."
+	kill ${SERVERPID}
         exit 1
     fi
     sleep 1
