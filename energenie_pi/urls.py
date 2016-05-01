e3ea0770fef6b00d27284fbcde32174e444f34c3
Simon Thorley <simon.thorley@thenom.local>
2016-05-01 15:58:14 +0100

    moved favicon to static

--- energenie_pi/urls.py
+++ energenie_pi/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^powersocket/', include('powersocket.urls', namespace="powersocket")),
+    url(r'^favicon.ico$', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False), name="favicon" ),
 ]
